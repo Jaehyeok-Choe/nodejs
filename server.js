1af773833afd36e8d16d393b33f781c48b7c757b
tbeben77 <tbeben77@gmail.com>
2021-01-26 00:20:25 +0900

    서버에서 HTML 받기

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const server = http.createServer((request, response)=>{
     console.log(request.url);
     console.log(request.method);
     fs.readFile("./index.html", null, (err,data)=>{
+        response.writeHead(200,{    // 200의 의미는 양호하다는 의미
+            "Content-type":"text/html"
+        });
+        
         response.write(data);
         response.end();
     })
