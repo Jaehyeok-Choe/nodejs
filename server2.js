60087fd8cd866afbaa2e407446672844cd29e9eb
tbeben77 <tbeben77@gmail.com>
2021-01-27 23:18:26 +0900

    NodeJS 4강 - Middleware 란, static 파일(css,html,javascript) 불러오기

--- server2.js
+++ server2.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static(__dirname + "/public"));
+
+app.use((req, res, next)=>{
+    next();
+})
+
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname + "/index.html");
+    res.status(200).sendFile(__dirname + "/index.html");
 })
 
 app.listen(3000,()=>{
